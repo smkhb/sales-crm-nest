068f27c95e4557e2d6607a7a1648784666338501
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-11-11 21:15:40 -0300

    Feat: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sales CRM Documentation')
+    .setDescription('Sales CRM description')
+    .setVersion('1.0')
+    .addTag('crm')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
